1b618fa7478afae02c8fe25b747c6966992dbec3
Daniel Gibeli <danielgibeli@gmail.com>
2020-05-01 12:06:00 -0300

    Atualização

---
+++ YmagiWebMvc/Data/YmagiWebMvcContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using YmagiWebMvc.Models;
+
+namespace YmagiWebMvc.Data
+{
+    public class YmagiWebMvcContext : DbContext
+    {
+        public YmagiWebMvcContext (DbContextOptions<YmagiWebMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<YmagiWebMvc.Models.Osc> Osc { get; set; }
+    }
+}
--- YmagiWebMvc/Startup.cs
+++ YmagiWebMvc/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using YmagiWebMvc.Data;
 
 namespace YmagiWebMvc
 {
@@ -33,6 +35,11 @@ namespace YmagiWebMvc
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<YmagiWebMvcContext>(options =>
+                    options.UseMySql(Configuration.GetConnectionString("YmagiWebMvcContext"), builder =>
+                        builder.MigrationsAssembly("YmagiWebMvc")));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
